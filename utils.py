1abe69bf44a0f4f2358fd315085597befbf8ac4f
Pasindu_SD <dilharawijesinghe86@gmail.com>
2025-12-13 23:49:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def ping_host(ip):
    result = subprocess.run(["ping", "-n", "1", "-w", "300", ip], stdout=subprocess.DEVNULL)   
    return result.returncode == 0
 
-def egt_hostname(ip):
+def get_hostname(ip):
    try:
       return socket.gethostbyaddr(ip)[0]
    except:
